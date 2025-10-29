39cc1d736903d3d238decac02e2d9368ffd38c1d
Bobby Gebert <bobgebert7@gmail.com>
2025-10-28 21:32:55 -0700

    Add input normalization using transforms

--- src/kaggle_digit_recognizer/dataset.py
+++ src/kaggle_digit_recognizer/dataset.py
@@ -9,7 +9,9 @@ class DigitDataset(Dataset[tuple[torch.Tensor, torch.Tensor]]):
     def __init__(self, data_path: Path):
         df = pd.read_csv(data_path)
         self.labels = torch.tensor(df["label"].values, dtype=torch.long)
-        self.images = torch.tensor(df.drop("label", axis=1).values, dtype=torch.float32)
+        self.images = (
+            torch.tensor(df.drop("label", axis=1).values, dtype=torch.float32) / 255.0
+        )
 
     def __len__(self) -> int:
         return len(self.labels)
