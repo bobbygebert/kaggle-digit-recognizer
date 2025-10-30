dd875d085e08f829e2fc2c4295ef527cb6b61261
Bobby Gebert <bobgebert7@gmail.com>
2025-10-29 18:53:21 -0700

    Replace RandomRotation with RandomAffine for better augmentation

--- src/kaggle_digit_recognizer/transforms.py
+++ src/kaggle_digit_recognizer/transforms.py
@@ -4,7 +4,7 @@ from torchvision import transforms
 def get_training_transform() -> transforms.Compose:
     return transforms.Compose(
         [
-            transforms.RandomRotation(15),
+            transforms.RandomAffine(degrees=15, translate=(0.1, 0.1), scale=(0.9, 1.1)),
             transforms.Normalize((0.5,), (0.5,)),
         ]
     )
