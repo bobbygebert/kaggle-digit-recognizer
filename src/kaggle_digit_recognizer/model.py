fc5336015b61283b2d14d56b2620af2158657576
Bobby Gebert <bobgebert7@gmail.com>
2025-10-29 20:12:28 -0700

    Add batch normalization after each convolutional layer

--- src/kaggle_digit_recognizer/model.py
+++ src/kaggle_digit_recognizer/model.py
@@ -9,14 +9,18 @@ class DigitRecognizer(nn.Module):
         super().__init__()
         self.model = nn.Sequential(
             nn.Conv2d(1, 16, kernel_size=3, padding=1),
+            nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.MaxPool2d(2),
             nn.Conv2d(16, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2),
             nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.Flatten(),
             nn.Linear(64 * 7 * 7, 10),
